341ac28cd835afd42ea06acdb66c26f7fca1cac7
Detishere <125731106+Detishere@users.noreply.github.com>
2023-02-25 15:13:23 +0530

    Update task 1.bat

--- task 1.bat
+++ task 1.bat
@@ -1,7 +1,3 @@
-set str1=Vihanga
-set str2= Neth
-set str3=27/3
-set str4= 1
-set str5= Colombo
+set input_file=C:\Users\IIT\Documents\IITwork\input.txt
 set message=%str1%%str2%,%str3%%str4%%str5%
 echo %message%
